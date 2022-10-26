1fbd0757293e0fb36324a56b0db1c10e995b3846
s362124 <s362124@oslomet.no>
2022-10-26 13:12:58 +0200

    Implementerte metoden "private void postordenRecursive" i oppgave 4

--- src/main/java/no/oslomet/cs/algdat/Oblig3/SBinTre.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig3/SBinTre.java
@@ -190,8 +190,16 @@ public class SBinTre<T> {
     }
 
     private void postordenRecursive(Node<T> p, Oppgave<? super T> oppgave) {
-
-        throw new UnsupportedOperationException("Ikke kodet ennå!");
+        if (p == null){
+            return;
+        }
+        if(p.venstre != null){
+            postordenRecursive(p.venstre, oppgave);
+        }
+        if (p.høyre != null){
+            postordenRecursive(p.høyre, oppgave);
+        }
+        oppgave.utførOppgave(p.verdi);
     }
 
     public ArrayList<T> serialize() {
