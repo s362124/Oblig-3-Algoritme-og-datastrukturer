d4240bc56538e28931d62cf8167cf5257659438f
s362124 <s362124@oslomet.no>
2022-10-26 19:58:29 +0200

    Implementerte metoden "public boolean fjern" i oppgave 6

--- src/main/java/no/oslomet/cs/algdat/Oblig3/SBinTre.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig3/SBinTre.java
@@ -104,11 +104,58 @@ public class SBinTre<T> {
         antall++;                                // én verdi mer i treet
         return true;                             // vellykket innlegging
 
-        // throw new UnsupportedOperationException("Ikke kodet ennå!");
     }
 
     public boolean fjern(T verdi) {
-        throw new UnsupportedOperationException("Ikke kodet ennå!");
+        if (verdi == null) return false;  // treet har ingen nullverdier
+
+        Node<T> p = rot, q = null;   // q skal være forelder til p
+
+        while (p != null)            // leter etter verdi
+        {
+            int cmp = comp.compare(verdi,p.verdi);      // sammenligner
+            if (cmp < 0) { q = p; p = p.venstre; }      // går til venstre
+            else if (cmp > 0) { q = p; p = p.høyre; }   // går til høyre
+            else break;    // den søkte verdien ligger i p
+        }
+        if (p == null) return false;   // finner ikke verdi
+
+        if (p.venstre == null || p.høyre == null)  // Tilfelle 1) og 2)
+        {
+            Node<T> b = p.venstre != null ? p.venstre : p.høyre;  // b for barn
+            if (p == rot) rot = b;
+
+            else if (p == q.venstre) {
+                q.venstre = b;
+                if (b!=null){
+                    b.forelder=q;
+                }
+            }
+            else {
+                q.høyre = b;
+            }
+
+            if(b!=null){
+                b.forelder = q;
+            }
+        }
+        else
+        {
+            Node<T> s = p, r = p.høyre;   // finner neste i inorden
+            while (r.venstre != null)
+            {
+                s = r;    // s er forelder til r
+                r = r.venstre;
+            }
+
+            p.verdi = r.verdi;   // kopierer verdien i r til p
+
+            if (s != p) s.venstre = r.høyre;
+            else s.høyre = r.høyre;
+        }
+        endringer++;
+        antall--;   // det er nå én node mindre i treet
+        return true;
     }
 
     public int fjernAlle(T verdi) {
